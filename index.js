015e51efe4544a6defef698a2167ef0661c677a8
haleytolar <Haleytolarwrites@outlook.com>
2024-02-07 08:49:42 -0600

    took user auth from /movies endpoint

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { se
 
 
 // search all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.get('/movies', async (req, res) => {
   await Movies.find()
       .then((movies) => {
           res.status(201).json(movies);
