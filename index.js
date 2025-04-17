8f3d4ea4687d483d1873cef7d096e7436991f3a9
haleytolar <Haleytolarwrites@outlook.com>
2025-04-17 13:10:10 -0500

    change db connection

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ const { check, validationResult } = require("express-validator");
 const bcrypt = require('bcrypt');
 
 // Connect to database
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 
 app.use(bodyParser.json());
